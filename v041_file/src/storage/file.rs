e3a87d19d3e8799cf08278b116a93e992e5ec4b5
Nathan Pinheiro <116799727+Nathan-Pinheiro@users.noreply.github.com>
2024-02-19 15:27:21 +0100

    Add files via upload

--- v041_file/src/storage/file.rs
+++ v041_file/src/storage/file.rs
@@ -1,6 +1,20 @@
 use std::sync::{Arc, RwLock};
+use crate::storage::Storage;
 
 pub struct FileStore {
     filepath: Arc<RwLock<String>>
 }
 
+#[async_trait::async_trait]
+impl Storage for FileStore {
+    async fn get_voting_machine(&self) -> Result<VotingMachine> {
+        let machine = self.machine.read().map_err(|_| anyhow!("Failed to acquire read lock"))?;
+        Ok(machine.clone())
+    }
+
+    async fn put_voting_machine(&mut self, machine: VotingMachine) -> Result<()> {
+        let mut write_guard = self.machine.write().map_err(|_| anyhow!("Failed to acquire write lock"))?;
+        *write_guard = machine;
+        Ok(())
+    }
+}
